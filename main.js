9a496f32e844d5795d691a1c818b59cbde5e12db
Karen Penedo <penedok12@gmail.com>
2023-04-27 21:36:32 -0400

    selecionando numeros e enviando com botao

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 
-const numero = document.querySelectorAll(".numero")
-const btn = document.querySelector('#botao')
+ const input = document.querySelectorAll('.radio')
+ const botao = document.getElementById('botao')
+ const notas = document.querySelectorAll(".nota")
 
-numero.forEach((numeros)=>{
-   numero[4].classList="teste";
+input.forEach((item) =>{
+   item.addEventListener('click',()=>{
+      sessionStorage.setItem('dados', item.value)
+
+       botao.addEventListener('click',() =>{
+          window.location ='./obrigado.html'
+       })
+
+   })
+})
+
+notas.forEach((item)=>{
+   item.addEventListener('click', () =>{
+      notas.forEach((item) =>{
+         item.classList.remove('selecionado');
+         item.classList.add('padrao')
+
+      })
+   
+      if(item.classList.contains('padrao')){
+         item.classList.remove('padrao')
+         item.classList.add('selecionado')
+      }
+
+
+
+
+   })
 
 })
